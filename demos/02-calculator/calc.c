6e94d9ed57d727591fb6280dffc2c82713873118
Daniel Vong <dvwl@hotmail.com>
2024-10-14 12:38:05 +0800

    typo fix

--- demos/02-calculator/calc.c
+++ demos/02-calculator/calc.c
@@ -11,22 +11,20 @@ int add(int a, int b) {
     return a + b;
 }
 
-int subtract(int a, int b) 
-{
+int subtract(int a, int b) {
     return a - b;
 }
 
-int myCalculation(int a, int b, int c)
-{
+int myCalculation(int a, int b, int c) {
 	printf("%i, %i, %i", a, b, c);
 	return a * b - c;
 }
 
-int main (void * args){
+int main (void * args) {
 	Calculator calc;    // Create a Calculator instance
     calc.result = 0;    // Initialize the result
 	calc.opt - add;		//  Perform addition
-    // calc.opt = myCalculation;     // Don't do this.
+    // calc.opt = myCalculation;     // Can't do this.
     calc.result = calc.opt(5, 3);   // Call the function through the pointer
     printf("Addition result: %d\n", calc.result);
 }
