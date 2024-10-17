62572d2d26d7b46c33180fd195cdbafa4a605a2c
Daniel Vong <dvwl@hotmail.com>
2024-10-17 12:50:37 +0800

    Create memory-mapped.c

---
+++ demos/07-memory-mapped/memory-mapped.c
@@ -0,0 +1,9 @@
+// Ensure that you installed the SDK
+// or have the right libraries in place
+#include <stdio.h>
+#include <windows.h>
+
+// On a Windows machine, if Line 4 shows
+// a red squiggly line means you don't
+// have the SDK installed correctly.
+// Check slides.
